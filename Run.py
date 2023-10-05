68976e8def9cb86077263856717e7880e4694e7e
T R A D E R <142751437+hamitrader@users.noreply.github.com>
2023-10-06 01:41:03 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -8,7 +8,7 @@ os.system('xdg-open https://chat.whatsapp.com/GsR7eXGFlrjIfQiBcc8dc7')
 os.system("clear")
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from HAMI import HAMI
+    from Dump import HAMI
     HAMI()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
